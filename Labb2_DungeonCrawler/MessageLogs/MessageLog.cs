34a535135cf95435c1654d76512448673bd15f16
bjornpetersson1 <bjornpetersson1@live.se>
2026-01-21 14:46:15 +0100

    Renamed folder

---
+++ Labb2_DungeonCrawler/MessageLogs/MessageLog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_DungeonCrawler.Log
+{
+    public class MessageLog
+    {
+        public List<string> MyLog { get; set; } = new();
+    }
+}
